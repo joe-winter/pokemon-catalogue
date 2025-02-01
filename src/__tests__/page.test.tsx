5e02d2034b27835bacac410038924f9cf13f6072
Joe Winter <joewinteruk@gmail.com>
2025-02-01 14:13:45 +0000

    set up jest

---
+++ src/__tests__/page.test.tsx
@@ -0,0 +1,10 @@
+import Home from '@/app/page'
+import '@testing-library/jest-dom'
+import { render, screen} from '@testing-library/react'
+
+
+describe('Home', () => {
+    it('is true', () => {
+        expect(true).toBe(true)
+    })
+})
