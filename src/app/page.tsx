5ea2472dc5bce6813718da368b79f9318002ba5c
Joe Winter <joewinteruk@gmail.com>
2025-02-04 11:43:51 +0000

    added loading spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import PokemonCard from "./components/PokemonCard";
 import PokemonService from "./services/pokemonService";
 import PageSwitcher from "./components/PageSwitcher";
 import SearchBar from "./components/SearchBar";
-
+import LoadingSpinner from "./components/LoadingSpinner";
 interface Pokemon {
   name: string;
   imageUrl: string;
@@ -18,6 +18,7 @@ interface PokemonUrl {
 }
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const [displayedPokemons, setDisplayedPokemons] = useState<Pokemon[]>([]);
   const [pageNumber, setPageNumber] = useState(0);
   const [initialPokemonList, setInitialPokemonList] = useState<PokemonUrl[]>([
@@ -35,7 +36,7 @@ export default function Home() {
       try {
         const pokemonData = await PokemonService.getAllPokemon();
         setInitialPokemonList(pokemonData);
-        setpokemonList(pokemonData)
+        setpokemonList(pokemonData);
       } catch (err) {
         console.log(err);
       }
@@ -47,12 +48,14 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-          const response = await PokemonService.getPokemonFromList(
-            pokemonList.slice(12 * pageNumber, 12 * (pageNumber + 1))
-          )
-          setDisplayedPokemons(response)
+        const response = await PokemonService.getPokemonFromList(
+          pokemonList.slice(12 * pageNumber, 12 * (pageNumber + 1))
+        );
+        setDisplayedPokemons(response);
       } catch (err) {
         console.log(err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchData();
@@ -64,13 +67,13 @@ export default function Home() {
       const filteredList = initialPokemonList.filter((pokemon) =>
         pokemon.name.toLowerCase().includes(searchValue.toLowerCase())
       );
-      setpokemonList(filteredList)
+      setpokemonList(filteredList);
       setSearchMessage(`Search results for "${searchValue}"`);
     }
   };
 
   return (
-    <div className="flex flex-col items-center font-[family-name:var(--font-geist-sans)]">
+    <div className="max-w-full flex flex-col items-center font-[family-name:var(--font-geist-sans)]">
       {/* title header */}
       <div className="flex flex-col border-b items-center mb-8 w-full">
         <h1 className="font-semibold text-4xl mt-12">Pokémon Browser</h1>
@@ -78,39 +81,52 @@ export default function Home() {
           Search and find Pokémon
         </h2>
       </div>
-      {/* search bar */}
-      <div className="flex items-center justify-between w-full px-44 mb-8">
-        <div className="font-semibold text-2xl">{searchMessage}</div>
-        <SearchBar
-          inputValue={searchValue}
-          setInputValue={setSearchValue}
-          placeholder="Find Pokémon"
-          searchFunction={handleSearch}
-        />
-      </div>
-      {/* pokemon grid */}
-      <div className="grid grid-cols-4 max-w-fit gap-x-4 gap-y-8">
-        {displayedPokemons.map((data, index) => (
-          <PokemonCard
-            key={index}
-            name={data.name}
-            imageUrl={data.imageUrl}
-            number={"#" + data.id.toString().padStart(4, "0")}
-            types={data.types}
+      {/* main content */}
+      <div className="w-full flex flex-col max-w-5xl">
+        {/* search bar */}
+        <div className="flex items-center justify-between  mb-8">
+          <div className="font-semibold text-2xl">{searchMessage}</div>
+          <SearchBar
+            inputValue={searchValue}
+            setInputValue={setSearchValue}
+            placeholder="Find Pokémon"
+            searchFunction={handleSearch}
           />
-        ))}
-      </div>
-      {/* page switcher */}
-      <div className="mt-8 flex">
-        <PageSwitcher
-          totalItems={pokemonList.length}
-          itemsPerPage={12}
-          pageNumber={pageNumber}
-          setPageNumber={setPageNumber}
-        />
+        </div>
+        {/* pokemon grid */}
+        <div className="mb-8">
+          {isLoading ? (
+            <div className="h-[1100px] flex">
+              <div className=" m-auto">
+                <LoadingSpinner />
+              </div>
+            </div>
+          ) : (
+            <div className="grid grid-cols-4 max-w-fit gap-x-4 gap-y-8">
+              {displayedPokemons.map((data, index) => (
+                <PokemonCard
+                  key={index}
+                  name={data.name}
+                  imageUrl={data.imageUrl !== null ? data.imageUrl : ""}
+                  number={"#" + data.id.toString().padStart(4, "0")}
+                  types={data.types}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+        {/* page switcher */}
+        <div className="m-8 flex justify-center">
+          <PageSwitcher
+            totalItems={pokemonList.length}
+            itemsPerPage={12}
+            pageNumber={pageNumber}
+            setPageNumber={setPageNumber}
+          />
+        </div>
       </div>
       {/* footer */}
-      <div className="flex flex-col border-t items-center mt-8 w-full">
+      <div className="flex flex-col border-t items-center mt-auto w-full">
         <h2 className="font-semibold  text-md mt-28 mb-16">
           Thank you for using Pokémon Browser!
         </h2>
