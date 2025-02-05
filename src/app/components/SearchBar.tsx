bb03d32f7d1de1ce25c4ffa0bc592765d49c6fd0
Joe Winter <joewinteruk@gmail.com>
2025-02-05 18:16:52 +0000

    search suggestiosn when typing

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,7 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { capitalizeString } from "@/lib/utils";
 import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
 
 interface SearchBarProps {
@@ -22,6 +21,17 @@ export default function SearchBar({
   searchList,
 }: SearchBarProps) {
   const [isOpen, setIsOpen] = useState(false);
+
+  const splitStringByInputValue = (element: string) => {
+    if (inputValue) {
+      // replace substring that matches input value with $#$
+      const replaced = element.replace(inputValue.toLowerCase(), "$#$");
+      // split at $ and remove an '' values
+      return replaced.split("$").filter((element) => element);
+    } else {
+      return [element];
+    }
+  };
   return (
     <div className="flex">
       <div className="w-48">
@@ -30,13 +40,37 @@ export default function SearchBar({
           value={inputValue}
           onChange={(e: ChangeEvent<HTMLInputElement>) => {
             setInputValue(e.target.value);
+            setIsOpen(true)
           }}
         />
         {isOpen && (
           <Card className="absolute Z-50 w-48 mt-2">
             {searchList.slice(0, 10).map((element, index) => (
               <CardContent className="px-2 py-0 text-lg" key={index}>
-                {capitalizeString(element)}
+                <button onClick={() => {
+                  setIsOpen(false)
+                  setInputValue(element)
+                }}>
+                  {splitStringByInputValue(element).map((element, index) =>
+                    element === "#" ? (
+                      <span
+                        key={index}
+                        className={`font-semibold ${
+                          index === 0 ? "capitalize" : ""
+                        }`}
+                      >
+                        {inputValue}
+                      </span>
+                    ) : (
+                      <span
+                        key={index}
+                        className={`${index === 0 ? "capitalize" : ""}`}
+                      >
+                        {element}
+                      </span>
+                    )
+                  )}
+                </button>
               </CardContent>
             ))}
           </Card>
